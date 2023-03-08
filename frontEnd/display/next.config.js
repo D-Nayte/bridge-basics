6e90be786fec3c8e952124a24b3c98b680d65aa9
Andy Schunke <schunke.andy@gmail.com>
2023-03-08 17:06:43 +0100

    dynamic link ind display and redirect to client

--- frontEnd/display/next.config.js
+++ frontEnd/display/next.config.js
@@ -4,7 +4,6 @@ const ENV = require("dotenv").config({
   path: path.resolve(__dirname, "../../.env"),
 });
 
-console.log("ENV :>> ", ENV);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,25 +3,12 @@ import path from "path";
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 import { Server, Origins } from "../node_modules/boardgame.io/server";
 import { Bridge } from "../../shared/lib/Bridge";
-import { networkInterfaces } from "os";
+import { getServerIp } from "./utils";
 
-const nets = networkInterfaces();
-const results = Object.create(null); // Or just '{}', an empty object
+const serverIp = getServerIp();
+console.log("serverIp1 :>> ", serverIp);
 
-for (const name of Object.keys(nets)) {
-  for (const net of nets[name]) {
-    // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-    // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
-    const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
-    if (net.family === familyV4Value && !net.internal) {
-      if (!results[name]) {
-        results[name] = [];
-      }
-      results[name].push(net.address);
-    }
-  }
-}
-console.log("results :>> ", results);
+const port: number = parseInt(process.env.NEXT_PUBLIC_SERVER_PORT);
 
 const server = Server({
   games: [Bridge],
@@ -31,5 +18,9 @@ const server = Server({
   ],
 });
 
-const port: number = parseInt(process.env.NEXT_PUBLIC_SERVER_PORT);
+server.router.post("/serverIp", (ctx, next) => {
+  console.log("serverIp2 :>> ", serverIp);
+  ctx.body = { serverIp: serverIp };
+});
+
 server.run(port);
