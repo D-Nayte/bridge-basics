92cb77986037c42a41f9fefc529158c92190e133
Andy Schunke <schunke.andy@gmail.com>
2023-03-13 11:29:13 +0100

    fix server not starting

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,14 @@
 import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
-import { Server, Origins } from "../node_modules/boardgame.io/server";
-import { Bridge } from "../../shared/src/lib/Bridge";
+import { Server, Origins } from "../../node_modules/boardgame.io/server";
 import { getServerIp } from "./utils";
+import { Bridge } from "../../shared/src/lib/Bridge";
 
 const serverIp = getServerIp();
 console.log("serverIp1 :>> ", serverIp);
 
-const port: number = parseInt(process.env.NEXT_PUBLIC_SERVER_PORT);
+const port: number = parseInt(process.env.NEXT_PUBLIC_SERVER_PORT || "8080");
 
 const server = Server({
   games: [Bridge],
--- server/src/utils.ts
+++ server/src/utils.ts
@@ -5,7 +5,7 @@ export const getServerIp = () => {
   const results = [];
 
   for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
+    for (const net of nets[name] || []) {
       // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
       // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
       const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
