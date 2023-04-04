fc4752b052547a0c68787f09c230724f9db2726d
Andy Schunke <schunke.andy@gmail.com>
2023-04-04 18:02:18 +0200

    refactor: delete unused components from demo/

--- shared/src/utils/urls.ts
+++ shared/src/utils/urls.ts
@@ -2,9 +2,9 @@ import { URLS } from "@interface";
 
 export const getURLs = (protocol: string): URLS => {
   const prodMode = process.env.NEXT_PUBLIC_NODE_ENV === "production";
-  const k8sAdress = process.env.NEXT_PUBLIC_Server_ADDRESS;
+  const k8sAdress = process.env.NEXT_PUBLIC_SERVER_ADDRESS;
   const devDomain = process.env.NEXT_PUBLIC_DEV_IP;
-  console.log("protocol :>> ", protocol);
+
   const serverPort: string | undefined = process.env.NEXT_PUBLIC_SERVER_PORT;
   const clientPort: string | undefined = process.env.NEXT_PUBLIC_CLIENT_PORT;
   const displayPort: string | undefined = process.env.NEXT_PUBLIC_DISPLAY_PORT;
